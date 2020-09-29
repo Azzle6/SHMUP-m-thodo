c24ea22827f1537e913ca9a349f4be3a90a97c2e
Eliott <eliott.chauchard@orange.fr>
2020-09-29 16:12:30 +0200

    Auto stash before merge of "Main" and "origin/Main"

---
+++ SHMUP methodo/Assets/Scripts/ELC_Explosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ELC_Explosion : MonoBehaviour
+{
+    private Animator animator;
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        StartCoroutine("PlayAndDestroy");
+    }
+
+    IEnumerator PlayAndDestroy()
+    {
+        animator.SetBool("Activate", true);
+        yield return new WaitForSeconds(0.5f);
+        Object.DestroyImmediate(this.gameObject);
+    }
+}
