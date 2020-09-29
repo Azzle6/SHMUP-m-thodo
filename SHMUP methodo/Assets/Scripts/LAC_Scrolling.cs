3e6a35535d4e17e3cfeb9914a4b06fb5a160728d
Luac <lo.accard@gmail.com>
2020-09-29 14:45:07 +0200

    Scroll manager

--- SHMUP methodo/Assets/Scripts/LAC_Scrolling.cs
+++ SHMUP methodo/Assets/Scripts/LAC_Scrolling.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class LAC_Scrolling : MonoBehaviour
 {
+    public float scrollSpeed;
+    public Transform[] childObj;
+
+    Camera cam;
+    public float camLimit;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // get and disable all child execpt scroll manager (child[0])
+        childObj = GetComponentsInChildren<Transform>();
+        for(int i = 0; i < childObj.Length; i++)
+        {
+            if (i !=0)
+            childObj[i].gameObject.SetActive(false);
+        }
+        // get camera
+        GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        cam = mainCamera.GetComponent<Camera>();
+
+        // set cam limit
+        float camHeight = camHeight = 1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).y - 0.5f);
+        camLimit = (camHeight / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // move the scroll manager
+        transform.position += new Vector3 (0, -scrollSpeed * Time.deltaTime,0);
+
+        // check what childObj to active
+        Active(ObjInScreen(childObj,camLimit));
+    }
+
+    public Transform ObjInScreen( Transform[] tList, float yScreen)
+    {
+        Transform transform = null;
+        foreach(Transform t in tList)
+        {
+            if (t.position.y < yScreen)
+                transform = t;
+        }
+
+        return transform;
+    }
+
+    public void Active( Transform child)
+    {
+        child.gameObject.SetActive(true);
+        child.SetParent(null);
     }
 }
