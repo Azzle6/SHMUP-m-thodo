b9a822bc97fdac4f1e084862e77fbdaef06ec1c0
Eliott <eliott.chauchard@orange.fr>
2020-09-29 12:02:48 +0200

    Display pour toutes les valeurs

---
+++ SHMUP methodo/Assets/Scripts/ELC_CentralDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ELC_CentralDisplay : MonoBehaviour
+{
+    public GameObject CentralTextObject;
+    public GameObject ScoreTextObject;
+    public GameObject LivesTextObject;
+    public ELC_PlayerStats playerStatsScript;
+    public bool isActivated;
+
+    private void Start()
+    {
+        isActivated = false;
+    }
+
+    private void Update()
+    {
+        DisplayScore();
+        DisplayLives();
+        if (isActivated) CentralTextObject.gameObject.SetActive(true);
+        else CentralTextObject.gameObject.SetActive(false);
+    }
+
+    private void DisplayScore()
+    {
+        ScoreTextObject.GetComponent<Text>().text = "Score : " + playerStatsScript.Score.ToString();
+    }
+
+    private void DisplayLives()
+    {
+        LivesTextObject.GetComponent<Text>().text = "Lives : " + playerStatsScript.Lives.ToString();
+    }
+
+    public void DisplayMessage(string message)
+    {
+        isActivated = true;
+        CentralTextObject.GetComponent<Text>().text = message;
+    }
+}
---
+++ SHMUP methodo/Assets/Scripts/ELC_PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ELC_PlayerStats : MonoBehaviour
+{
+    public int Lives;
+    public float Score;
+    public ELC_CentralDisplay displayScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lives = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Lives <= 0)
+        {
+            displayScript.DisplayMessage("Game Over");
+            Object.Destroy(this.gameObject);
+        }
+    }
+}
